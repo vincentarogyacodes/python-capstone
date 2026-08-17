fc223624f8e42076fdda1371eec8a4f05e090572
Vincent Arogya <vincent.sick@gmail.com>
2026-08-17 20:21:46 +0530

    Chore: minor maintainence

--- isolated.py
+++ isolated.py
@@ -1,12 +1,2 @@
-def load_purchases(filename):
-    records = []
-    with open (filename, 'r') as file:
-        for line in file:
-            parts = line.strip().split(",")
-            record = parts[0], parts[1], parts[2]
-            records.append(record)
-            print(record)
-        return record
-
-result = load_purchases("data.txt")
-print(result)
+numbers = {1, 2, 2, 3, 3, 3}
+print(numbers)
